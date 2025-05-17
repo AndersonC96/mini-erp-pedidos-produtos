6017b95e8112a77e6b082bfcbf7869dd2bb4ba45
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-17 17:37:55 -0300

    Create lista.php

---
+++ app/views/produtos/lista.php
@@ -0,0 +1,34 @@
+<?php require '../app/views/shared/header.php'; ?>
+<div class="container mt-4">
+    <h2>Lista de Produtos</h2>
+    <a href="index.php?rota=produto_form" class="btn btn-primary mb-3">Novo Produto</a>
+    <?php if (!empty($produtos)) : ?>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($produtos as $produto): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($produto['nome']) ?></td>
+                        <td>R$ <?= number_format($produto['preco'], 2, ',', '.') ?></td>
+                        <td>
+                            <form action="index.php?rota=adicionar_carrinho" method="POST" class="d-inline">
+                                <input type="hidden" name="produto_id" value="<?= $produto['id'] ?>">
+                                <input type="number" name="quantidade" value="1" class="form-control d-inline w-25" min="1">
+                                <button class="btn btn-sm btn-success">Comprar</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p>Nenhum produto cadastrado.</p>
+    <?php endif ?>
+</div>
+<?php require '../app/views/shared/footer.php'; ?>
