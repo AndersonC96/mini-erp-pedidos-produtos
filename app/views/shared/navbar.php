d1e8e2b31fada2109e972ab87674cab8b1679b2e
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-17 18:18:59 -0300

    Create navbar.php

---
+++ app/views/shared/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php?rota=produtos">Mini ERP</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php?rota=produtos">Produtos</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?rota=produto_form">Novo Produto</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?rota=carrinho">Carrinho</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?rota=cupons">Cupons</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
