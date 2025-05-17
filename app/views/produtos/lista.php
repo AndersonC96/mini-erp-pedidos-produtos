e1d36a9c3057368034bf49a3b29325a4c6e52e2a
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-17 19:40:03 -0300

    Update lista.php

--- app/views/produtos/lista.php
+++ app/views/produtos/lista.php
@@ -1,7 +1,9 @@
 <?php require '../app/views/shared/header.php'; ?>
+
 <div class="container mt-4">
     <h2>Lista de Produtos</h2>
     <a href="index.php?rota=produto_form" class="btn btn-primary mb-3">Novo Produto</a>
+
     <?php if (!empty($produtos)) : ?>
         <table class="table table-bordered">
             <thead>
@@ -17,11 +19,12 @@
                         <td><?= htmlspecialchars($produto['nome']) ?></td>
                         <td>R$ <?= number_format($produto['preco'], 2, ',', '.') ?></td>
                         <td>
-                            <form action="index.php?rota=adicionar_carrinho" method="POST" class="d-inline">
+                            <form action="index.php?rota=adicionar_carrinho" method="POST" class="d-inline me-2">
                                 <input type="hidden" name="produto_id" value="<?= $produto['id'] ?>">
-                                <input type="number" name="quantidade" value="1" class="form-control d-inline w-25" min="1">
+                                <input type="number" name="quantidade" value="1" min="1" class="form-control d-inline w-25 me-2" style="width: 70px; display: inline-block;">
                                 <button class="btn btn-sm btn-success">Comprar</button>
                             </form>
+                            <a href="index.php?rota=produto_editar&id=<?= $produto['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
                         </td>
                     </tr>
                 <?php endforeach ?>
@@ -31,4 +34,5 @@
         <p>Nenhum produto cadastrado.</p>
     <?php endif ?>
 </div>
+
 <?php require '../app/views/shared/footer.php'; ?>
