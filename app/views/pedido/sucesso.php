ae2a84acf5107c19f1f8d79142fbedd43bbaf970
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-17 18:35:52 -0300

    Delete sucesso.php

--- app/views/pedido/sucesso.php
+++
@@ -1,7 +0,0 @@
-<?php require '../app/views/shared/header.php'; ?>
-<div class="container mt-4 text-center">
-    <h2 class="text-success">Pedido finalizado com sucesso!</h2>
-    <p>Obrigado por sua compra. Em breve você receberá um e-mail com os detalhes do pedido.</p>
-    <a href="index.php?rota=produtos" class="btn btn-primary mt-3">Continuar comprando</a>
-</div>
-<?php require '../app/views/shared/footer.php'; ?>
