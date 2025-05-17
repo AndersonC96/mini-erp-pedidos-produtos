2c9c7cc4cc3716d9ceda6cebdea3dd3da06b062e
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-17 17:29:07 -0300

    Create Cupom.php

---
+++ app/models/Cupom.php
@@ -0,0 +1,18 @@
+<?php
+    require_once '../config/database.php';
+    class Cupom {
+        public static function salvar($dados) {
+            global $conn;
+            $codigo = $conn->real_escape_string($dados['codigo']);
+            $valor = floatval($dados['valor_desconto']);
+            $minimo = floatval($dados['minimo_subtotal']);
+            $validade = $conn->real_escape_string($dados['validade']);
+            $conn->query("INSERT INTO cupons (codigo, valor_desconto, minimo_subtotal, validade) VALUES ('$codigo', $valor, $minimo, '$validade')");
+        }
+        public static function validar($codigo, $subtotal) {
+            global $conn;
+            $codigo = $conn->real_escape_string($codigo);
+            $res = $conn->query("SELECT * FROM cupons WHERE codigo = '$codigo' AND validade >= CURDATE() AND minimo_subtotal <= $subtotal LIMIT 1");
+            return $res->fetch_assoc();
+        }
+    }
