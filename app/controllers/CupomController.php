0d4090bcfd9ef6494b2a369e98dac18f64e893da
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-18 11:12:05 -0300

    Update CupomController.php

--- app/controllers/CupomController.php
+++ app/controllers/CupomController.php
@@ -9,4 +9,17 @@
             header('Location: index.php?rota=cupons');
             exit;
         }
+        public function validar() {
+            header('Content-Type: application/json');
+            require_once '../app/models/Cupom.php';
+            $cupom = $_POST['cupom'] ?? '';
+            $subtotal = floatval($_POST['subtotal'] ?? 0);
+            $cupomObj = Cupom::validar($cupom, $subtotal);
+            if ($cupomObj) {
+                echo json_encode(['valido' => true, 'desconto' => floatval($cupomObj['valor_desconto'])]);
+            } else {
+                echo json_encode(['valido' => false, 'mensagem' => 'Cupom inválido, expirado ou subtotal abaixo do mínimo.']);
+            }
+            exit;
+        }
     }
