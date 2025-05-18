995be9bf9030faa3236daa20112e912ceb7da6b6
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2025-05-18 17:31:54 -0300

    Update navbar.php

--- app/views/shared/navbar.php
+++ app/views/shared/navbar.php
@@ -1,16 +1,38 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg shadow-sm" style="background-color: #ffffff;">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php?rota=produtos">Mini ERP</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <a class="navbar-brand fw-bold" href="index.php?rota=produtos" style="color: #2563eb;">
+            <i class="bi bi-box-seam"></i> Mini ERP
+        </a>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="index.php?rota=produtos">Produtos</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?rota=estoque">Estoque</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?rota=carrinho">Carrinho</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?rota=pedidos">Pedidos</a></li>
-                <li class="nav-item"><a class="nav-link" href="index.php?rota=cupons_listar">Cupons</a></li>
+                <li class="nav-item">
+                    <a class="nav-link fw-semibold px-3" href="index.php?rota=produtos" style="color: #2d3436;">
+                        <i class="bi bi-bag"></i> Produtos
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-semibold px-3" href="index.php?rota=estoque" style="color: #2d3436;">
+                        <i class="bi bi-archive"></i> Estoque
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-semibold px-3" href="index.php?rota=carrinho" style="color: #2d3436;">
+                        <i class="bi bi-cart3"></i> Carrinho
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-semibold px-3" href="index.php?rota=pedidos" style="color: #2d3436;">
+                        <i class="bi bi-receipt"></i> Pedidos
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-semibold px-3" href="index.php?rota=cupons_listar" style="color: #2d3436;">
+                        <i class="bi bi-ticket-perforated"></i> Cupons
+                    </a>
+                </li>
             </ul>
         </div>
     </div>
